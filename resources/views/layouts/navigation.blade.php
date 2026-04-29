25511bf69c0a7ea80487bf33d9fe6effde3ae487
farkhanisturkia <farkhan22032000@gmail.com>
2026-04-29 10:59:18 +0700

    [update] change class

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                     </svg>
-                    <span id="notif-count" class="hidden absolute top-1 right-1 flex items-center justify-center h-5 w-5 text-[10px] font-bold text-white bg-red-600 rounded-full ring-2 ring-zinc-300 dark:ring-zinc-900">
+                    <span id="notif-count" class="hidden absolute top-1 right-1 items-center justify-center h-5 w-5 text-[10px] font-bold text-white bg-red-600 rounded-full ring-2 ring-zinc-300 dark:ring-zinc-900">
                         0
                     </span>
                 </button>
